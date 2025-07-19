f258d43e13c7156808f00838d71db14ba6a24aad
ParmarAayush <parmarayush1816@gmail.com>
2025-07-19 14:44:34 +0530

    add name attribute

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
 import Home from "./pages/Home.jsx";
 import Login from "./features/auth/Login.jsx";
 import Register from "./features/auth/Register.jsx";
